cb38af9ce8346a6d9311516b2f05e78d6cef633d
151250040 <151250040@smail.nju.edu.cn>
2019-08-06 15:40:32 +0800

    8.6

--- src/interview/alibaba/findMinOfBst.java
+++ src/interview/alibaba/findMinOfBst.java
@@ -1,4 +1,32 @@
 package interview.alibaba;
 
+import java.util.Stack;
+
 public class findMinOfBst {
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+    }
+    int res = 0;
+    public int findMinK(TreeNode node, int k){
+        TreeNode cur = node;
+        Stack<TreeNode> stack =new Stack();
+        while(cur!=null || !stack.empty()){
+            if (cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode temp = stack.pop();
+                k--;
+                if (k == 0){
+                    return temp.val;
+                }
+                if (temp.right!=null){
+                    cur = temp.right;
+                }
+            }
+        }
+        return -1;
+    }
 }
